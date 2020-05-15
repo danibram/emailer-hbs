87033f3ae29895bb6824e1f737d95c5e2c7bd4d0
dbr <info@dbr.io>
2020-05-15 18:54:15 +0200

    fixes and update deps

--- dist/Mailer.js
+++ dist/Mailer.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.emailer = exports.Emailer = void 0;
 const fs = require("fs");
 const Handlebars = require("handlebars");
 const juice = require("juice");
--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,19 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !exports.hasOwnProperty(p)) __createBinding(exports, m, p);
+}
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.emailer = exports.Emailer = void 0;
 const Mailer_1 = require("./Mailer");
-exports.Emailer = Mailer_1.Emailer;
-exports.emailer = Mailer_1.emailer;
+Object.defineProperty(exports, "Emailer", { enumerable: true, get: function () { return Mailer_1.Emailer; } });
+Object.defineProperty(exports, "emailer", { enumerable: true, get: function () { return Mailer_1.emailer; } });
+__exportStar(require("./Types"), exports);
 exports.default = Mailer_1.emailer;
 //# sourceMappingURL=index.js.map
